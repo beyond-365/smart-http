4e69cbf73f4a797020adf3db82edd5d417e90fd3
zhangxiang <zhangxiang_php@vchangyi.com>
2021-10-12 14:07:06 +0800

    :zap: 响应中间件

--- src/Kernel/Middleware/ResponseMiddleware.php
+++ src/Kernel/Middleware/ResponseMiddleware.php
@@ -4,7 +4,32 @@
 namespace Beyond\SmartHttp\Kernel\Middleware;
 
 
-class ResponseMiddleware
-{
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
+abstract class ResponseMiddleware extends BaseMiddleware
+{
+    /**
+     * @param RequestInterface $request
+     * @param ResponseInterface $response
+     * @return ResponseInterface
+     */
+    public function handle(RequestInterface $request, ResponseInterface $response)
+    {
+        return $response->withHeader($this->getHeaderKey(), $this->getHeaderValue($request));
+    }
+
+    /**
+     * @param callable $handler
+     * @return \Closure
+     */
+    function __invoke(callable $handler)
+    {
+        return function (RequestInterface $request, array $options) use ($handler) {
+            $promise = $handler($request, $options);
+            return $promise->then(function (ResponseInterface $response) use ($request) {
+                return $this->handle($request, $response);
+            });
+        };
+    }
 }
