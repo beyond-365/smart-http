1b1bf275ed7acefb98e748812126823d4ecc1845
zhangxiang <zhangxiang_php@vchangyi.com>
2021-10-17 18:25:40 +0800

    :children_crossing: 请求异常处理

--- src/Sample/Demo/DemoClient.php
+++ src/Sample/Demo/DemoClient.php
@@ -5,11 +5,20 @@ namespace Beyond\SmartHttp\Sample\Demo;
 
 
 use Beyond\SmartHttp\Kernel\BaseClient;
+use Beyond\SmartHttp\Kernel\Exceptions\AuthorizationException;
+use Beyond\SmartHttp\Kernel\Exceptions\BadRequestException;
 use Beyond\SmartHttp\Kernel\Exceptions\InvalidArgumentException;
+use Beyond\SmartHttp\Kernel\Exceptions\ResourceNotFoundException;
+use Beyond\SmartHttp\Kernel\Exceptions\ServiceInvalidException;
+use Beyond\SmartHttp\Kernel\Exceptions\ValidationException;
 use Beyond\SmartHttp\Kernel\ServiceContainer;
 use Beyond\SmartHttp\Sample\Middleware\AfterMiddle;
 use Beyond\SmartHttp\Sample\Middleware\DemoMiddleware;
 use Beyond\SmartHttp\Sample\Middleware\SignMiddleware;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Exception\ServerException;
+use Psr\Http\Message\ResponseInterface;
 
 abstract class DemoClient extends BaseClient
 {
@@ -32,6 +41,57 @@ abstract class DemoClient extends BaseClient
         $this->pushMiddlewareByKey(SignMiddleware::getAccessName());
         $this->pushMiddlewareByKey(DemoMiddleware::getAccessName());
         $this->pushMiddlewareByKey(AfterMiddle::getAccessName());
+    }
 
+    /**
+     * 处理请求异常 request
+     *
+     * @param $url
+     * @param string $method
+     * @param array $options
+     * @return ResponseInterface
+     * @throws AuthorizationException
+     * @throws BadRequestException
+     * @throws ResourceNotFoundException
+     * @throws ServiceInvalidException
+     * @throws ValidationException
+     */
+    public function request($url, $method = 'POST', array $options = [])
+    {
+        try {
+            return parent::request($url, $method, $options);
+        } catch (ClientException $e) {
+            $response = $e->getResponse();
+            $statusCode = $response->getStatusCode();
+            $contents = $response->getBody()->getContents();
+            $content = json_decode($contents);
+
+            $message = property_exists($content, 'message') ? $content->message : '';
+            $code = property_exists($content, 'code') ? $content->code : 100000;
+
+            switch ($statusCode) {
+                case 404:
+                    throw new ResourceNotFoundException($message, $code);
+                case 400:
+                case 422:
+                    throw new ValidationException($message, $code);
+                    break;
+                case 401:
+                    throw new AuthorizationException($message, $code);
+                default:
+                    throw new ServiceInvalidException($message ? $message : 'Service Invalid', $code);
+            }
+        } catch (ServerException $e) {
+            $response = $e->getResponse();
+            $contents = $response->getBody()->getContents();
+            $content = json_decode($contents);
+            $message = property_exists($content, 'message') ? $content->message : 'Service Invalid';
+            $code = property_exists($content, 'code') ? $content->code : 100000;
+
+            throw new ServiceInvalidException($message, $code);
+        } catch (RequestException $e) {
+            // 在发送网络错误(连接超时、DNS错误等)时
+            throw new BadRequestException($e->getMessage(), 400);
+        }
     }
 }
